91c04f63baa66052cd213a71da3a1055a9506bbf
alif <alifalamin4@gmail.com>
2019-02-05 13:38:39 +0600

    route and for loop

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/profile', function(req, res, next) {
+  res.render('author');
+});
+
+router.get('/timeline', function(req, res, next) {
+  var data = [
+    { id: 1, name: "bob" },
+    { id: 2, name: "john" },
+    { id: 3, name: "jake" },
+  ];
+
+  res.render('time',{info:data});
+});
+
+
+
 module.exports = router;
